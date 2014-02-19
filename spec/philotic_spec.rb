0c0b0848676a0d73bc0d0a43867c68b8b602eb7f
Nathan Keyes <nkeyes@connamara.com>
2014-02-19 11:26:36 -0600

    testing updates

--- spec/philotic_spec.rb
+++ spec/philotic_spec.rb
@@ -9,12 +9,6 @@ describe Philotic do
 
   describe '.initialize_named_queue!' do
 
-    after(:all) do
-      test_queues.each_pair do |queue_name, config|
-        Philotic.delete_queue(queue_name, config)
-      end
-    end
-
     let(:test_queues) do
       {
           app_live_feed: {
@@ -44,20 +38,36 @@ describe Philotic do
       end
     end
 
-    it 'should set up the queue with the right parameters' do
-      ENV['INITIALIZE_NAMED_QUEUE'] = 'true'
+it 'should set up the queue with the right parameters' do
+  ENV['INITIALIZE_NAMED_QUEUE'] = 'true'
 
+  test_queues.each_pair do |queue_name, config|
 
-      test_queues.each_pair do |queue_name, config|
-        config[:bindings].each do |arguments|
-          expect_any_instance_of(AMQP::Queue).to receive(:bind).with(instance_of(AMQP::Exchange), { arguments: arguments })
-          Philotic.initialize_named_queue! queue_name, config
-        end
+    queue_options = Philotic::DEFAULT_NAMED_QUEUE_OPTIONS.dup
+    queue_options.merge!(config[:options] || {})
 
-        done((test_queues.keys.length + 1) * 0.1) do
+    channel_double = double(AMQP::Channel)
+    queue_double = double(AMQP::Queue)
+    exchange_double = double(AMQP::Exchange)
 
-        end
-      end
+    expect(Philotic).to receive(:connect!).and_yield.exactly(2).times
+    expect(AMQP).to receive(:channel).and_return(channel_double).exactly(2).times
+    expect(channel_double).to receive(:queue).with(queue_name, queue_options).and_yield(queue_double).exactly(2).times
+    expect(queue_double).to receive(:delete).and_yield
+    expect(Philotic::Connection).to receive(:close).and_yield
+    expect(channel_double).to receive(:headers).with(config[:exchange], durable: true) { exchange_double }
+    expect(AMQP).to receive(:channel) { channel_double }
+    expect(queue_double).to receive(:name) { queue_name }
+
+    config[:bindings].each do |arguments|
+      expect(queue_double).to receive(:bind).with(exchange_double, { arguments: arguments })
+      Philotic.initialize_named_queue! queue_name, config
     end
+
+    done((test_queues.keys.length + 1) * 0.1) do
+
+    end
+  end
+end
   end
 end
