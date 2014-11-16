bbde3c0c22f54f2add4a2bfd3a4188adfd378c86
Nathan Keyes <nathan.keyes@avantcredit.com>
2014-11-16 12:27:15 -0600

    refactor connect!

--- lib/philotic/connection.rb
+++ lib/philotic/connection.rb
@@ -13,46 +13,40 @@ module Philotic
       Philotic::Config
     end
 
-
     def connect!
       if connected?
         Philotic.logger.info "already connected to RabbitMQ; host:#{config.rabbit_host}"
-        yield if block_given?
         return
       end
 
-      connection_settings = {
+      start_connection!
+
+      if connected?
+        Philotic.logger.info "connected to RabbitMQ; host:#{config.rabbit_host}"
+        set_exchange_return_handler!
+        true
+      else
+        Philotic.logger.warn "failed connected to RabbitMQ; host:#{config.rabbit_host}"
+        false
+      end
+    end
+
+    def start_connection!
+      @connection = Bunny.new(config.rabbit_url, connection_settings)
+      @connection.start
+    end
+
+    def connection_settings
+      {
           timeout:                   config.timeout.to_i,
           automatically_recover:     true,
           on_tcp_connection_failure: config.connection_failed_handler,
       }
-
-      @connection = Bunny.new(config.rabbit_url, connection_settings)
-      @connection.start
-
-      if connected?
-        Philotic.logger.info "connected to RabbitMQ; host:#{config.rabbit_host}"
-      else
-        Philotic.logger.warn "failed connected to RabbitMQ; host:#{config.rabbit_host}"
-      end
-
-      #@connection.on_tcp_connection_loss do |cl, settings|
-      #  config.method(:connection_loss_handler).call.call(cl, settings)
-      #end
-      #
-      #@connection.after_recovery do |conn, settings|
-      #  Philotic.logger.info "Connection recovered, now connected to #{config.rabbit_host}"
-      #end
-
-      setup_exchange_handler!
-      yield if block_given?
-
     end
 
     def close
       Philotic.logger.info "closing connection to RabbitMQ; host:#{config.rabbit_host}"
       connection.close if connected?
-      yield if block_given?
     end
 
     def connected?
@@ -67,7 +61,7 @@ module Philotic
       @exchange ||= channel.headers(config.exchange_name, durable: true)
     end
 
-    def setup_exchange_handler!
+    def set_exchange_return_handler!
       exchange.on_return do |basic_return, metadata, payload|
         config.method(:message_return_handler).call.call(basic_return, metadata, payload)
       end
