5489247ca3dd7d1760baae5451e22a0fe94d76e3
Nathan Keyes <nathan.keyes@avantcredit.com>
2015-05-31 18:32:24 -0500

    start of Philotic::Consumer

--- lib/philotic/consumer.rb
+++ lib/philotic/consumer.rb
@@ -3,29 +3,79 @@ require 'philotic/subscriber'
 module Philotic
   class Consumer < Philotic::Subscriber
 
-    def self.subscribe_to(subscription)
-      @subscription = subscription
-    end
+    class << self
 
-    def self.subscribe
-      new(Philotic.connection).tap do |instance|
-        instance.subscribe
+      def subscribe_to(subscription)
+        @subscription = subscription
       end
-    end
 
-    def self.subscription
-      @subscription
+      def subscription
+        @subscription
+      end
+
+      def ack_messages
+        @ack_messages = true
+      end
+
+      def exclusive
+        @exclusive = true
+      end
+
+      def ack_messages?
+        !!@ack_messages
+      end
+
+      def exclusive?
+        !!@exclusive
+      end
+
+      def requeueable_errors(*errors)
+        @requeueable_errors ||= Set.new
+        @requeueable_errors.merge errors
+      end
+
+      def rejectable_errors(*errors)
+        @rejectable_errors ||= Set.new
+        @rejectable_errors.merge errors
+      end
+
+      def subscribe
+        new(Philotic.connection).tap do |instance|
+          instance.subscribe
+        end
+      end
+
+      def subscription_options
+        {
+          manual_ack: ack_messages?,
+          exclusive:  exclusive?,
+        }
+      end
     end
 
     def subscribe
-      super(self.class.subscription) do |event, metadata|
-        consume(event)
+      super(self.class.subscription, self.class.subscription_options) do |message|
+        if self.class.ack_messages?
+          begin
+            consume(message)
+          rescue => e
+            case e.class
+              when *self.class.requeueable_errors
+                reject(message, true)
+              when *self.class.rejectable_errors
+                reject(message, false)
+              else
+                raise e
+            end
+          end
+          acknowledge(message)
+        else
+          consume(message)
+        end
       end
-      Philotic.endure
-      self
     end
 
-    def consume(event)
+    def consume(message)
       raise NotImplementedError
     end
   end
